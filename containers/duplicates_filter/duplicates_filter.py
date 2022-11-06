036ff4ab873a5ff88036ffae5392bc2b6bb32976
Drasungor <aguscambiano@gmail.com>
2022-11-06 12:46:25 -0300

    Start duplication filter refactorization

--- containers/duplicates_filter/duplicates_filter.py
+++ containers/duplicates_filter/duplicates_filter.py
@@ -56,3 +56,101 @@ def message_callback(middleware, input_message):
 
 middleware = middleware.ChannelChannelFilter(RABBIT_HOST, INPUT_QUEUE, OUTPUT_QUEUE, message_callback)
 middleware.run()
+
+
+class DuplicationFilter:
+    def __init__(self):
+        self.middleware = middleware.ChannelChannelFilter(RABBIT_HOST, INPUT_QUEUE, OUTPUT_QUEUE, self.process_received_message)
+        self.clients_sent_videos = {} # key: client_id, value: sent_videos_set
+        self.received_eofs = 0
+        self.previous_stage_size = self.middleware.get_previous_stage_size()
+
+        signal.signal(signal.SIGTERM, self.__handle_signal)
+
+    def process_received_message(self, ch, method, properties, body):
+        line = json.loads(body)
+
+        if method.routing_key == general_config["general_subscription_routing_key"]:
+            self.received_eofs += 1
+            if self.received_eofs == self.previous_stage_size:
+                self.has_to_close = True
+        else:
+            video_id = line[local_config["indexes"]["video_id"]]
+            title = line[local_config["indexes"]["title"]]
+            category = line[local_config["indexes"]["category"]]
+            if not (video_id in self.sent_videos):
+                self.sent_videos.add(video_id)
+                self.middleware.send({ "type": cluster_type, "tuple": (video_id, title, category) })
+
+        if self.has_to_close:
+            self.middleware.send_general(None)
+            self.middleware.close()
+            print("Closed MOM")
+
+    def start_received_messages_processing(self):
+        self.middleware.run()
+
+    def __handle_signal(self, *args): # To prevent double closing 
+        self.has_to_close = True
+
+def main():
+    # logging.basicConfig(
+    #     format='%(asctime)s %(levelname)-8s %(message)s',
+    #     level="DEBUG",
+    #     datefmt='%Y-%m-%d %H:%M:%S',
+    # )
+    wrapper = DuplicationFilter()
+    wrapper.start_received_messages_processing()
+
+if __name__ == "__main__":
+    main()
+
+
+# class DuplicationFilter:
+#     def __init__(self):
+#         self.middleware = MOM(cluster_type, self.process_received_message)
+#         self.sent_videos = set()
+#         self.received_eofs = 0
+#         self.has_to_close = False
+#         self.is_processing_message = False
+#         self.previous_stage_size = self.middleware.get_previous_stage_size()
+
+#         signal.signal(signal.SIGTERM, self.__handle_signal)
+
+#     def process_received_message(self, ch, method, properties, body):
+#         line = json.loads(body)
+
+#         if method.routing_key == general_config["general_subscription_routing_key"]:
+#             self.received_eofs += 1
+#             if self.received_eofs == self.previous_stage_size:
+#                 self.has_to_close = True
+#         else:
+#             video_id = line[local_config["indexes"]["video_id"]]
+#             title = line[local_config["indexes"]["title"]]
+#             category = line[local_config["indexes"]["category"]]
+#             if not (video_id in self.sent_videos):
+#                 self.sent_videos.add(video_id)
+#                 self.middleware.send({ "type": cluster_type, "tuple": (video_id, title, category) })
+
+#         if self.has_to_close:
+#             self.middleware.send_general(None)
+#             self.middleware.close()
+#             print("Closed MOM")
+
+#     def start_received_messages_processing(self):
+#         self.middleware.start_received_messages_processing()
+
+#     def __handle_signal(self, *args): # To prevent double closing 
+#         self.has_to_close = True
+
+# def main():
+#     # logging.basicConfig(
+#     #     format='%(asctime)s %(levelname)-8s %(message)s',
+#     #     level="DEBUG",
+#     #     datefmt='%Y-%m-%d %H:%M:%S',
+#     # )
+#     wrapper = DuplicationFilter()
+#     wrapper.start_received_messages_processing()
+
+# if __name__ == "__main__":
+#     main()

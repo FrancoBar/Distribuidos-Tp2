cbca5c6577334759eb27002a566aa7dc940e5e96
Drasungor <aguscambiano@gmail.com>
2022-11-07 22:24:27 -0300

    Remove base filter from on_recv and on_last function calls in brodcast_copies

--- containers/common/broadcast_copies.py
+++ containers/common/broadcast_copies.py
@@ -8,11 +8,11 @@ def broadcast_copies(base_filter, input_message, id, copies, on_recv=None, on_la
         base_filter.put_back(input_message)
         if not on_recv:
             return None
-        return on_recv(base_filter, input_message)
+        return on_recv(input_message)
 
     if len(input_message[PROCESS_LIST_COLUMN]) == copies:
         if on_last:
-            return on_last(base_filter, input_message)
+            return on_last(input_message)
 
         return None
 
@@ -21,7 +21,7 @@ def broadcast_copies(base_filter, input_message, id, copies, on_recv=None, on_la
         base_filter.put_back(input_message)
         if not on_recv:
             return None
-        return on_recv(base_filter, input_message)
+        return on_recv(input_message)
 
     base_filter.put_back(input_message)
     return None

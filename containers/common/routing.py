3f83c77c8815609041958509be1d9b8e8db02b5b
Drasungor <aguscambiano@gmail.com>
2022-11-09 22:39:28 -0300

    Add general routing function

--- containers/common/routing.py
+++ containers/common/routing.py
@@ -14,3 +14,17 @@ def router(message, control_route_key, next_stage_name, hashing_attributes, next
         return [control_route_key]
     stage_routing_key = f'{next_stage_name}-{utils.hash_fields(message, hashing_attributes) % next_stage_amount}'
     return [stage_routing_key]
+
+
+def router_iter(message, control_route_key, next_stages_data): # message, control_route_key, [{"next_stage_name":..., "hashing_attributes":..., "next_stage_amount":...},...]
+    if message['type'] == 'control':
+        return [control_route_key]
+
+    stage_routing_keys = []
+    for stage_data in next_stages_data:
+        next_stage_name = stage_data["next_stage_name"]
+        hashing_attributes = stage_data["hashing_attributes"]
+        next_stage_amount = stage_data["next_stage_amount"]
+        aux_routing_key = f'{next_stage_name}-{utils.hash_fields(message, hashing_attributes) % next_stage_amount}'
+        stage_routing_keys.append(aux_routing_key)
+    return stage_routing_keys

7cd24b955b4cae59e117b252428cf789b7b65a38
Drasungor <aguscambiano@gmail.com>
2022-11-14 19:30:25 -0300

    Refactor code and add eof counting

--- containers/all_countries_agg/all_countries_agg.py
+++ containers/all_countries_agg/all_countries_agg.py
@@ -27,46 +27,64 @@ NODE_ID = config['ALL_COUNTRIES_AGG']['node_id']
 CONTROL_ROUTE_KEY = config['GENERAL']['control_route_key']
 PORT = int(config['ALL_COUNTRIES_AGG']['port'])
 FLOWS_AMOUNT = int(config['ALL_COUNTRIES_AGG']['flows_amount'])
+MIN_DAYS = int(config['ALL_COUNTRIES_AGG']['min_days'])
 
 PREVIOUS_STAGE_AMOUNT = config['ALL_COUNTRIES_AGG']['previous_stage_amount'] # Hacer un for de las etapas anteriores
 NEXT_STAGE_AMOUNT = config['ALL_COUNTRIES_AGG']['next_stage_amount'] # Hacer un for de las etapas anteriores
 NEXT_STAGE_NAME = config['ALL_COUNTRIES_AGG']['next_stage_name'] # Hacer un for de las etapas anteriores
 
-aux_client_id = 'generic_client_id'
+# aux_client_id = 'generic_client_id'
 
 class CountriesAmountFilter:
     def __init__(self):
         self.middleware = middleware.ExchangeExchangeFilter(RABBIT_HOST, INPUT_EXCHANGE, OUTPUT_EXCHANGE, f'ALL_COUNTRIES_AGG-{NODE_ID}', 
                                                     CONTROL_ROUTE_KEY, OUTPUT_EXCHANGE, routing.router, self.process_received_message)
-        self.clients_received_eofs = {} # key: client_id, value: number of eofs received
         # self.previous_stage_size = self.middleware.get_previous_stage_size()
+        self.clients_received_eofs = {} # key: client_id, value: number of eofs received
         self.clients_countries_per_day = {} # key: client_id, value: {key: video_id, value: { key: day, value: countries set}}
         self.clients_countries_amount = {} # key: client_id, value: countries_amount
 
-    def _on_recv_eof(self, input_message):
-        client_id = aux_client_id
-        if client_id in self.clients_countries_amount:
-            del self.clients_countries_amount[client_id]
-        if client_id in self.clients_countries_per_day:
-            del self.clients_countries_per_day[client_id]
+    # def _on_recv_eof(self, input_message):
+    #     # client_id = aux_client_id
+    #     client_id = input_message['client_id']
+    #     if client_id in self.clients_countries_amount:
+    #         del self.clients_countries_amount[client_id]
+    #     if client_id in self.clients_countries_per_day:
+    #         del self.clients_countries_per_day[client_id]
     
-        # del self.clients_max_day[client_id]
-        return None
+    #     # del self.clients_max_day[client_id]
+    #     return None
 
-    def _on_last_eof(self, input_message):
-        utils.clear_all_files(STORAGE)
-        return {'type':'control', 'case':'eof'}
+    # def _on_last_eof(self, input_message):
+    #     return {'type':'control', 'case':'eof'}
 
-    def _on_recv_config(self, input_message):
-        client_id = aux_client_id
-        self.clients_countries_amount[client_id] = int(input_message['amount_countries'])
-        return None
+    # def _on_recv_config(self, input_message):
+    #     # client_id = aux_client_id
+    #     client_id = input_message['client_id']
+    #     self.clients_countries_amount[client_id] = int(input_message['amount_countries'])
+    #     return None
+
+    def process_control_message(self, input_message):
+        client_id = input_message['client_id']
+        if input_message['case'] == 'eof':
+            self.clients_received_eofs[client_id] += 1
+            if self.clients_received_eofs[client_id] == PREVIOUS_STAGE_AMOUNT:
+                del self.clients_countries_amount[client_id]
+                del self.clients_countries_per_day[client_id]
+                del self.clients_received_eofs[client_id]
+                return input_message
+            else:
+                return None
+        else:
+            self.clients_countries_amount[client_id] = int(input_message['amount_countries'])
+            return input_message
 
     def all_countries_agg(self, input_message):
-        client_id = aux_client_id
+        # client_id = aux_client_id
+        client_id = input_message['client_id']
         client_countries_amount = self.clients_countries_amount[client_id]
-        if not (client_id in self.clients_countries_per_day):
-            self.clients_countries_per_day[client_id] = {}
+        # if not (client_id in self.clients_countries_per_day):
+        #     self.clients_countries_per_day[client_id] = {}
         client_trending_days_dict = self.clients_countries_per_day[client_id]
 
         temp=time.strptime(input_message['trending_date'], '%Y-%m-%dT%H:%M:%SZ')
@@ -102,22 +120,24 @@ class CountriesAmountFilter:
         return output_message
 
     def process_received_message(self, input_message):
-        client_id = aux_client_id
+        client_id = input_message['client_id']
+        message_to_send = None
+
+        # Initialization
+        if not (client_id in self.clients_received_eofs):
+            self.clients_received_eofs[client_id] = 0
+            self.clients_countries_per_day[client_id] = {}
+
+        # Message processing
         if input_message['type'] == 'data':
-            if not (client_id in self.clients_countries_amount):
-            # if amount_countries == INVALID_AMOUNT:
-                #Put back data message until config message
-                self.middleware.put_back(input_message)
-                return None
-            else:
-                return self.all_countries_agg(input_message)
+            message_to_send = self.all_countries_agg(input_message)
         else:
-            if input_message['case'] == 'eof':
-                return broadcast_copies.broadcast_copies(self.middleware, input_message, ID, COPIES, self._on_recv_eof, self._on_last_eof)
-            else:
-                return broadcast_copies.broadcast_copies(self.middleware, input_message, ID, COPIES, self._on_recv_config, None)
-                
-            return None
+            message_to_send = self.process_control_message(input_message)
+
+        # Result communication
+        if message_to_send != None:
+            self.middleware.send(message_to_send)
+
 
     def start_received_messages_processing(self):
         self.middleware.run()

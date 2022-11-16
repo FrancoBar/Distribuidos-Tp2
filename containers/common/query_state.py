b0ee95beb3f97a7f4005059d3855bd087ecdc9ed
FrancoBar <fbarreneche@fi.uba.ar>
2022-11-16 14:47:31 -0300

    Merge t and w into w

--- containers/common/query_state.py
+++ containers/common/query_state.py
@@ -2,7 +2,6 @@ import os
 
 FILE_TYPE = '.csv'
 
-TABLE_BYTE = 't'
 WRITE_BYTE = 'w'
 COMMIT_BYTE = 'c'
 
@@ -24,10 +23,11 @@ class QueryState:
                 query_id = query_name[:-len(FILE_TYPE)]
                 self._queries[query_id] = self._build_query(query_name)
             except FileNotFoundError as e:
-                return
+                continue
 
     def _build_query(self, query_name):
-        query = {'id':0 , 'msg_table' : {}, 'values' : {}}
+        #Starts at -1 to compensate for rare empty files cases
+        query = {'id':-1 , 'msg_table' : {}, 'values' : {}}
         last_commit_size = 0
         pending_lines = []
         with open(self._storage + query_name, 'r+') as query_file:
@@ -38,26 +38,32 @@ class QueryState:
                     break
 
                 log_type = line[0]
-                if log_type == WRITE_BYTE or log_type == TABLE_BYTE:
+                if log_type == WRITE_BYTE:
                     pending_lines.append(line)
                 elif log_type == COMMIT_BYTE and len(pending_lines) > 0:
                     last_id = query['id']
                     update_table = {}
                     update_value = {}
-                    for pending_line in pending_lines:
-                        try:
-                            if pending_line[0] == TABLE_BYTE:
-                                log_type, origin, in_id, out_id = pending_line[:-1].split(',')
+                    try:
+                        for pending_line in pending_lines:
+                            args = pending_line[:-1].split(',')
+                            if len(args) == 4:
+                                log_type, origin, in_id, out_id = args
                                 update_table[origin] = in_id
                                 query['id'] = int(out_id)
-                            else:
-                                log_type, key, value = pending_line[:-1].split(',')
+                            elif len(args) == 6:
+                                log_type, origin, in_id, out_id, key, value = args
+                                update_table[origin] = in_id
+                                query['id'] = int(out_id)
                                 update_value[key] = self._read_value(query['values'], key, value)
-                        except ValueError:
-                            query['id'] = last_id
-                            print('Corrupted line')
-                            query_file.truncate(last_commit_size)
-                            break
+                            else:
+                                print(len(args))
+                                pass          
+                    except ValueError:
+                        query['id'] = last_id
+                        print('Corrupted line')
+                        query_file.truncate(last_commit_size)
+                        break
                     pending_lines = []
                     query['values'].update(update_value)
                     query['msg_table'].update(update_table)
@@ -86,7 +92,7 @@ class QueryState:
     def get_id(self, query_id):
         return self._get_query(query_id)['id']
 
-    def is_msg_received(self, query_id, origin, msg_id):
+    def is_last_msg(self, query_id, origin, msg_id):
         assert(type(msg_id) == str)
         query = self._get_query(query_id)
         if origin not in query['msg_table']:
@@ -97,21 +103,23 @@ class QueryState:
         with open(self._storage + str(query_id) + FILE_TYPE, 'a') as query_file:
             query_file.write(line)
 
-    def prepare(self, query_id, origin, msg_id):
+    def write(self, query_id, origin, msg_id, key=None, value=None):
         query = self._get_query(query_id)
         out_id = query['id']
-        self._write(query_id, TABLE_BYTE + ',{},{},{}\n'.format(origin, msg_id, out_id))
-        
+        log_header = '{},{},{},{}'.format(WRITE_BYTE, origin, msg_id, out_id)
+        if not key or not value:
+            self._write(query_id, log_header + '\n')
+        else:
+            log_body = ',{},{}\n'.format(key, self._write_value(query['values'], key, value))
+            self._write(query_id, log_header + log_body)
+
+    def commit(self, query_id, origin, msg_id):
+        query = self._get_query(query_id)
+        out_id = query['id']
+        self._write(query_id, COMMIT_BYTE + '\n')
+
         query['msg_table'][origin] = msg_id
         query['id'] = out_id + 1
-
-    def write(self, query_id, key, value):
-        query = self._get_query(query_id)
-        out_id = query['id']
-        self._write(query_id, '{},{},{}\n'.format(WRITE_BYTE, key, self._write_value(query['values'], key, value)))
-
-    def commit(self, query_id):
-        self._write(query_id, COMMIT_BYTE + '\n')
 
     def __str__(self):
             return str(self._queries)
@@ -126,27 +134,27 @@ state = QueryState('./storage/', _default_read_value, _default_write_value)
 
 print(state)
 
-# state.prepare('cliente1', 'origin1', 1)
-# state.write('cliente1', 'key1',20)
-# state.write('cliente1', 'key2',40)
-# state.commit('cliente1')
+# state.write('cliente1', 'origin1', 1, 'key1',20)
+# state.commit('cliente1', 'origin1', 1)
 
-# state.prepare('cliente1', 'origin1', 2)
-# state.write('cliente1', 'key1',10)
-# state.write('cliente1', 'key3',50)
-# state.commit('cliente1')
+# state.write('cliente1', 'origin2', 8)
+# state.commit('cliente1', 'origin2', 8)
 
-# state.prepare('cliente2', 'origin1', 3)
-# state.write('cliente2', 'key4',10)
-# state.commit('cliente2')
+# state.write('cliente1', 'origin1', 3, 'key1',3)
+# state.commit('cliente1', 'origin1', 3)
 
-print(state.get_id('cliente1'), state.get_id('cliente2'))
-print(state.get_values('cliente1'), state.get_values('cliente2'))
+# state.write('cliente2', 'origin1', 1, 'key2',5)
+# state.commit('cliente2', 'origin1', 1)
 
-# print(state.is_msg_received('cliente1','origin2', '3'))
-# print(state.is_msg_received('cliente1','origin2', '2'))
-# print(state.is_msg_received('cliente1','origin1', '4'))
-# print(state.is_msg_received('cliente1','origin1', 8))
+# print(state.get_id('cliente1'), state.get_id('cliente2'))
+# print(state.get_values('cliente1'), state.get_values('cliente2'))
+
+# print(state.is_last_msg('cliente1','origin1', '1'))
+# print(state.is_last_msg('cliente1','origin1', '2'))
+# print(state.is_last_msg('cliente1','origin1', '3'))
+# print(state.is_last_msg('cliente1','origin1', '4'))
+# print(state.is_last_msg('cliente1','origin2', '8'))
+#print(state.is_last_msg('cliente1','origin2', 8))
 
 #state.delete_query('cliente1')
 

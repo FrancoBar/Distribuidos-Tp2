d5682503a37480d3801a3cb574d7cc59a4be70a3
FrancoBar <fbarreneche@fi.uba.ar>
2022-11-17 18:04:41 -0300

    Add retries to mitigate udp deficientcies

--- containers/test_dind/test_dind.py
+++ containers/test_dind/test_dind.py
@@ -9,6 +9,7 @@ import socket
 
 SERVICES = os.environ['SERVICES'].split(',')
 SLEEP_SECONDS = 1
+RETRIES = 3
 
 health_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 health_socket.bind(('', 2114))
@@ -20,14 +21,20 @@ logging.basicConfig(level=logging.INFO)
 logging.info(SERVICES)
 while True:
 	for service in SERVICES:
-		try:
-			health_socket.sendto(b'A', (service, 2114))
-			#Check address == IMAGE
-			byte_array, address = health_socket.recvfrom(1)
-			logging.info(service + ', ' + str(address) + ', ' + str(byte_array))
-		except (socket.gaierror, socket.timeout):
-			logging.error(service)
-			subprocess.run(['docker', 'restart', service], check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			#logging.info('Command executed. Result={}. Output={}. Error={}'.format(result.returncode, result.stdout, result.stderr))
-		
+		retries = 0
+		while True:
+			try:
+				health_socket.sendto(b'A', (service, 2114))
+				#Check address == IMAGE
+				byte_array, address = health_socket.recvfrom(1)
+				logging.info(service + ', ' + str(address) + ', ' + str(byte_array))
+				break
+			except (socket.gaierror, socket.timeout):
+				retries = retries + 1
+				logging.info('Retry sending to ' + service)
+				if retries >= RETRIES:
+					logging.error(service)
+					subprocess.run(['docker', 'restart', service], check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					#logging.info('Command executed. Result={}. Output={}. Error={}'.format(result.returncode, result.stdout, result.stderr))
+					break
 	time.sleep(SLEEP_SECONDS)
--- containers/test_target/test_target.py
+++ containers/test_target/test_target.py
@@ -3,14 +3,20 @@ import os
 import sys
 
 FAIL_AFTER = int(os.environ['FAIL_AFTER'])
+SKIP_AFTER = int(os.environ['SKIP_AFTER'])
 
 health_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 health_socket.bind(('', 2114))
 
 i = 0
 while(True):
-	byte_array, address = health_socket.recvfrom(1)
-	health_socket.sendto(byte_array, address)
-	i = i + 1
-	if i >= FAIL_AFTER:
+	if FAIL_AFTER > 0 and i >= FAIL_AFTER:
 		sys.exit(1)
+
+	byte_array, address = health_socket.recvfrom(1)
+
+	if SKIP_AFTER > 0 and i >= SKIP_AFTER:
+		i = 0
+	else:
+		health_socket.sendto(byte_array, address)
+		i = i + 1

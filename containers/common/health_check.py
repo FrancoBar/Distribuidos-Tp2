3f05e943fed8886bbc3d20f07f77a38e579b9270
Drasungor <aguscambiano@gmail.com>
2022-12-02 17:30:03 -0300

    Add reuse addr config on accepter socket creation

--- containers/common/health_check.py
+++ containers/common/health_check.py
@@ -5,41 +5,55 @@ import sys
 import time
 import logging
 import os
+from .utils import initialize_config, initialize_log
 import socket
 from .transmition_udp import send_uint32, recv_uint32
 
+config = initialize_config()
+LOGGING_LEVEL = config['GENERAL']['logging_level']
+initialize_log(LOGGING_LEVEL)
+
+
 SLEEP_SECONDS = 1
-TIMEOUT = 2
+TIMEOUT = 0.25
 HEALTH_ECHO_PORT = 2114
 HEALTH_MONITOR_PORT = 4114
 
 def monitor(service_list, max_retries):
-	health_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-	health_socket.bind(('', HEALTH_MONITOR_PORT))
-	health_socket.settimeout(TIMEOUT)
-	i = 0
+	try:
+		health_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+		health_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		health_socket.bind(('', HEALTH_MONITOR_PORT))
+		health_socket.settimeout(TIMEOUT)
+		i = 0
 
-	while True:
-		for service in service_list:
-			for retries in range(max_retries):
-				try:
-					i = i + 1
-					send_uint32(health_socket, i, (service, HEALTH_ECHO_PORT))
-					while True:
-						n, address = recv_uint32(health_socket)
-						if i == n:
-							logging.debug('{}, {}, {}'.format(n, service, address))
-							break
+		while True:
+			for service in service_list:
+				for retries in range(max_retries):
+					try:
+						i = i + 1
+						send_uint32(health_socket, i, (service, HEALTH_ECHO_PORT))
+						while True:
+							n, address = recv_uint32(health_socket)
+							if i == n:
+								logging.info('{}, {}, {}'.format(n, service, address))
+								break
+							else:
+								logging.info('{} != {}'.format(i, n))
+						break
+					except (socket.gaierror, socket.timeout):
+						logging.info(f'retries: {retries}')
+						logging.info(f'max_retries: {max_retries}')
+						if retries >= max_retries - 1:
+							result = subprocess.run(['docker', 'restart', service], check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+							logging.info('Command executed. Result={}. Output={}. Error={}'.format(result.returncode, result.stdout, result.stderr))
 						else:
-							logging.debug('{} != {}'.format(i, n))
-					break
-				except (socket.gaierror, socket.timeout):
-					if retries >= max_retries - 1:
-						result = subprocess.run(['docker', 'restart', service], check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-						logging.debug('Command executed. Result={}. Output={}. Error={}'.format(result.returncode, result.stdout, result.stderr))
-					else:
-						logging.debug('Retry sending to ' + service)
-		time.sleep(SLEEP_SECONDS)
+							logging.info('Retry sending to ' + service)
+			time.sleep(SLEEP_SECONDS)
+	except Exception as e:
+		logging.exception(e)
+	finally:
+		health_socket.close()
 
 def echo_server():
 	health_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
